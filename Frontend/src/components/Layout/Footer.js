2fa72b4bed1d9a311edfdd7a23db1988ec761723
sampathreddygoluguri <146423494+sampathreddygoluguri@users.noreply.github.com>
2024-09-02 16:24:28 +0530

    Add files via upload

---
+++ Frontend/src/components/Layout/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+
+  <footer>
+        <p className="text-center">
+            RetroServe -2024, All rights reserved
+        </p>
+        </footer>
+      
+    </>
+  )
+}
+
+export default Footer
